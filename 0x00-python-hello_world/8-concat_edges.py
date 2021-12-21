84d78d4a6e1674d3f62c15bf84b50eacf11795da
BigDany1792 <dany1792dota@gmail.com>
2021-12-20 21:59:38 -0500

    chore: delete strings literal and integrated concatenation of the strings

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = "object-oriented programming with Python"
+str = str[39:55] + str[55:67] + str[-22:-17] + str[0:6]
 print(str)
