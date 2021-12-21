ce0bb37ce54e0027706e9f67325c8b98230b876d
BigDany1792 <85207977+BigDany1792@users.noreply.github.com>
2021-12-20 22:25:21 -0500

    ci
     added function that checks if a singly linked list has a cycle in it

--- 0x00-python-hello_world/10-check-cycle.c
+++ 0x00-python-hello_world/10-check-cycle.c
@@ -1,20 +1,23 @@
 #include "lists.h"
 /**
- * comments betty
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *before = list;
-	listint_t *after = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-	while (after && after->next)
+	if (!list)
+		return (0);
+	while (slow && fast && fast->next)
 	{
-		before = before->next;
-		after = after->next->next;
-		if (before == after)
-		{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 			return (1);
-		}
 	}
 	return (0);
 }
