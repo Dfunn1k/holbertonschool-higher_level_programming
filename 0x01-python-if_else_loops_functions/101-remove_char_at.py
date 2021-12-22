8ca00073748f2e54eb9bbe000131ad8e6bddad0d
BigDany1792 <dany1792dota@gmail.com>
2021-12-21 23:59:28 -0500

    fix: fix the conditional in line 5

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,7 @@
 def remove_char_at(str, n):
     new_str = ""
     for i in str:
-        if (n > 0 and n <= len(str)):
+        if (n >= 0 and n <= len(str)):
             if i != str[n]:
                 new_str += i
         else:
