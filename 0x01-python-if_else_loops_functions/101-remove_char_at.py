62f86b282243982d1d4a80e4ecdaf86f31567e77
BigDany1792 <dany1792dota@gmail.com>
2021-12-21 23:57:47 -0500

    refactor: more organized code

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
+    new_str = ""
     for i in str:
-        if n <= len(str) and n > 0:
-            if i == str[n]:
-                continue
-            print("{}".format(i), end="")
+        if (n > 0 and n <= len(str)):
+            if i != str[n]:
+                new_str += i
         else:
-            print("{}".format(i), end="")
-    return("\0")
+            return(str)
+    return(new_str)
