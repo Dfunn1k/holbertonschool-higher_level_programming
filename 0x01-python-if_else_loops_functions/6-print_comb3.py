b8e77906d8a02e9c384b878ba5dfe5fb5640093e
BigDany1792 <dany1792dota@gmail.com>
2021-12-22 12:55:56 -0500

    chore: change two lines for one

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 for num1 in range(0, 10):
     for num2 in range(0, 10):
-        if num2 < num1:
-            continue
-        elif num1 == num2:
-            continue
+        if num2 <= num1:
+            pass
         else:
             if (num1 == 8 and num2 == 9):
                 print("{}{}".format(num1, num2))
