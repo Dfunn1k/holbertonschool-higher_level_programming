5a3763621aae5aa29d467a6f28cb5440a63c18de
BigDany1792 <dany1792dota@gmail.com>
2021-12-22 13:00:31 -0500

    chore: optimized code

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,12 +3,11 @@ def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
-            continue
         if i % 3 == 0:
             print("Fizz", end=" ")
-            continue
         if i % 5 == 0:
             print("Buzz", end=" ")
-            continue
 
         print("{}".format(i), end=" ")
+
+fizzbuzz()
