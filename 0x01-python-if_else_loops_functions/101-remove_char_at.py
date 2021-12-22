f90288defc76656bf3edb6b15dfe992577ce0457
BigDany1792 <dany1792dota@gmail.com>
2021-12-21 23:25:51 -0500

    fix: index out range

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     for i in str:
-        if i == str[n]:
-            continue
-        print("{}".format(i), end="")
+        if n <= len(str) and n > 0:
+            if i == str[n]:
+                continue
+            print("{}".format(i), end="")
+        else:
+            print("{}".format(i), end="")
     return("\0")
