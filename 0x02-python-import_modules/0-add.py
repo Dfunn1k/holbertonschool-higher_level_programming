df28cd015152704ced5666317104a0c298aaa5e2
BigDany1792 <dany1792dota@gmail.com>
2021-12-23 12:40:28 -0500

    style: fix pycodestyle

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
+
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
