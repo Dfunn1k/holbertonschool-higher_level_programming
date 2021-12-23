09fa8e834eef91eec3125df85fead893bdd6df2c
BigDany1792 <dany1792dota@gmail.com>
2021-12-23 12:33:24 -0500

    fix: use __name__

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import add_0
-a = 1
-b = 2
+if __name__ =="__main__":
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
 
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+
