c8713955db1b2e1df7b4a4cca70317039a78a750
BigDany1792 <dany1792dota@gmail.com>
2021-12-23 15:30:40 -0500

    fix: delete first print

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,6 @@
 import hidden_4
 
 if __name__ == "__main__":
-    print(dir(hidden_4))
     for i in range(0, len(dir(hidden_4))):
         if (dir(hidden_4)[i][0]) != '_':
             print(dir(hidden_4)[i])
