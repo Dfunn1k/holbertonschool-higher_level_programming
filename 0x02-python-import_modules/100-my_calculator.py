547b2865eaaa0569b88b1047821e9f1a2ad130aa
BigDany1792 <dany1792dota@gmail.com>
2021-12-28 18:11:58 -0500

    style: fix pycodestyle

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
         num2 = int(argv[3])
 
         if argv[2] == '+':
-            print("{:d} {:s} {:d} = {:d}".format(num1, argv[2], num2, add(num1, num2)))
+            print("{} {} {} = {}".format(num1, argv[2], num2, add(num1, num2)))
         elif argv[2] == '-':
-            print("{:d} {:s} {:d} = {:d}".format(num1, argv[2], num2, sub(num1, num2)))
+            print("{} {} {} = {}".format(num1, argv[2], num2, sub(num1, num2)))
         elif argv[2] == '*':
-            print("{:d} {:s} {:d} = {:d}".format(num1, argv[2], num2, mul(num1, num2)))
+            print("{} {} {} = {}".format(num1, argv[2], num2, mul(num1, num2)))
         elif argv[2] == '/':
-            print("{:d} {:s} {:d} = {:d}".format(num1, argv[2], num2, div(num1, num2)))
+            print("{} {} {} = {}".format(num1, argv[2], num2, div(num1, num2)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
