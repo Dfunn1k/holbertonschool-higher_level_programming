dd5888d27be3e8cbf3b416009d2e269728bb4760
BigDany1792 <dany1792dota@gmail.com>
2021-12-27 19:24:09 -0500

    style: fix

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -3,7 +3,6 @@ def magic_calculation(a, b, c):
     if a < b:
         return c
     if c > b:
-        return (a + b)
+        return a + b
     else:
-        return((a * b) - c)
-    
+        return a * b - c
