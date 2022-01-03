30f48072e992a1181d94bea529302d7948085968
BigDany1792 <dany1792dota@gmail.com>
2022-01-03 15:53:24 -0500

    chore: codi optimez and fix bugs

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    count = 0
     for items in matrix:
         for num in items:
-            print("{:d}".format(num), end=' ')
-        print()
+            if count == 2:
+                print("{:d}".format(num), end='')
+            else:
+                print("{:d}".format(num), end=' ')
+            count += 1
+        count = 0
+        print("")
