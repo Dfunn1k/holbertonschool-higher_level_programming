3ef30aaa43801e0e5fd18fa1232fcefdbbd1e369
BigDany1792 <dany1792dota@gmail.com>
2022-01-03 12:59:15 -0500

    ci: added None return  if the list is None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list == None:
+        return(None)
     for items in range(len(my_list) - 1, -1, -1):
         print("{:d}".format(my_list[items]))
