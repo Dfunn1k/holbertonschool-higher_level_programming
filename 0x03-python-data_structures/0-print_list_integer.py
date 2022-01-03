0b7ff25416549b2f10ec18d77412c262a371ea63
BigDany1792 <dany1792dota@gmail.com>
2022-01-03 00:55:13 -0500

    chore: optimized code

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for items in range(0, len(my_list)):
-        print("{}".format(my_list[items]))
+    for items in my_list:
+        print("{:d}".format(items))
