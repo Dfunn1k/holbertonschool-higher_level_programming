d2a5a1e900c781e92580d5d6e394f0cf2c124172
BigDany1792 <dany1792dota@gmail.com>
2022-01-17 16:22:15 -0500

    chore: added return None

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         cociente = a / b
     except ZeroDivisionError:
-        return None
+        cociente = None
     finally:
         print("Inside result: {}".format(cociente))
         return cociente
