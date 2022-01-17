91886857fa9a54dec4d10594bfd6be54132039a4
BigDany1792 <dany1792dota@gmail.com>
2022-01-17 14:25:27 -0500

    chore: change exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             count += 1
         print("")
-    except Exception:
+    except ValueError:
         pass
-    return count
+    finally:
+        return count
