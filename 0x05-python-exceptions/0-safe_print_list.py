53385ad6c601f34219a3749a9625963419c90f0d
BigDany1792 <dany1792dota@gmail.com>
2022-01-17 10:40:43 -0500

    ci: added function taht prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        print("")
+    except Exception:
+        pass
+    return count
