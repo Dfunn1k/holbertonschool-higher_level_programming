aeda1b3e2e3817b435aceb6040471aeb01b88fd2
BigDany1792 <dany1792dota@gmail.com>
2022-01-17 15:52:13 -0500

    chore: change structure

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,10 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
             count += 1
-        except:
+        except ValueError:
             pass
+        except TypeError:
+            pass
+
     print("")
     return count
