09987a0b64fdcc30af771bd827aa7d0fcb35601c
BigDany1792 <dany1792dota@gmail.com>
2022-01-17 15:51:10 -0500

    chore: change structure

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
+from multiprocessing.sharedctypes import Value
+
+
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(0, x):
+    for i in range(0, x):
+        try:
             print("{:d}".format(my_list[i]), end='')
             count += 1
-        print("")
-    except:
-        pass
-    finally:
-        if x > count:
-            return count
-        else:
-            return x
+        except:
+            pass
+    print("")
+    return count
