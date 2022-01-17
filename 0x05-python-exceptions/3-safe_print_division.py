2e8bb601b7f4486d8a1cfa0d01ee15874319f6c4
BigDany1792 <dany1792dota@gmail.com>
2022-01-17 16:03:26 -0500

    ci: function that divides two integers and prints the result

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        cociente = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {}".format(cociente))
+        return cociente
