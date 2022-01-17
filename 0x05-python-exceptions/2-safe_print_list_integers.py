d8ad2f3e5363bf4f1cac1067b98fc7bfd62a7845
BigDany1792 <dany1792dota@gmail.com>
2022-01-17 15:45:53 -0500

    chore: deleted specifif exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end='')
             count += 1
         print("")
-    except ValueError:
+    except:
         pass
     finally:
         if x > count:
