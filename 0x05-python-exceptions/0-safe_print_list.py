af5585527be94109cfb167a93ab0c0f7691066ab
BigDany1792 <dany1792dota@gmail.com>
2022-01-17 15:07:26 -0500

    style: fix for autopep 8

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,4 +8,7 @@ def safe_print_list(my_list=[], x=0):
     except ValueError:
         pass
     finally:
-        return count
+        if x > count:
+            return count
+        else:
+            return x
