23cc9ad1f197a3b4db0f8a70ecd1fab5aa4c6576
BigDany1792 <dany1792dota@gmail.com>
2022-01-23 21:36:38 -0500

    ci: added class 'square'

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Class about Square"""
+
+
+class Square():
+    """Define square"""
+    def __init__(self, size):
+        """
+        Args:
+            arg1 (int)
+        """
+        self._size = size
