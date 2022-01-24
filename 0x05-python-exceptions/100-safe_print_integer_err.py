78b9039089ba0ca0ab0ad124399e448ed7a11d73
BigDany1792 <dany1792dota@gmail.com>
2022-01-23 20:42:09 -0500

    ci: added exercises 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as err:
+        sys.stderr.write("Exception: {}\n".format(err))
