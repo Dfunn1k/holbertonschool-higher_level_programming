dae681609cbacea750d1f17525750fb4cbe4e533
BigDany1792 <dany1792dota@gmail.com>
2022-04-25 14:59:44 -0500

    fix error when the program printed

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str[39:66] + str[-23:-17] + str[:6])
+str = str[39:66] + str[-23:-17] + str[:6]
+print(str)
