0f44705914b0b891af5259b239d2c417d885166c
BigDany1792 <dany1792dota@gmail.com>
2022-04-28 13:29:55 -0500

    ci: added program that prints all the names defined by the compiled module 'hidden_4.pyc'

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from add_0 import hidden_4
+
+if __name__ == "__main__":
+    for i in range(0, len(dir(hidden_4))):
+        if dir(hidden_4)[i][0] != '_':
+            print("{}".format(dir(hidden_4)[i]))
