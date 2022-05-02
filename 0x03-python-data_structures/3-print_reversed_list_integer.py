c294d3865e29b4439ab0ad37eeb4a2e985060dd0
BigDany1792 <dany1792dota@gmail.com>
2022-05-02 17:03:06 -0500

    fix: fix problem with the list is NONE

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,8 @@
 def print_reversed_list_integer(my_list=[]):
     """prints all integers of a list, in
     reverse order"""
+    if my_list is None:
+        return None
     cpylist = my_list
     cpylist.reverse()
     for num in cpylist:
