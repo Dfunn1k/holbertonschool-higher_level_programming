f8e0f86fdd42f3723f50636f00fe45153fab55e0
BigDany1792 <dany1792dota@gmail.com>
2022-05-02 18:45:07 -0500

    ci: added function that prints a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     """prints a matrix of integers"""
 
     for rows in matrix:
-        print("".join([str(x) for x in rows]))
+        print("".join("{:d}".format(x) for x in rows))
