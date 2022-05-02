a57477055cbf9e7edbedfbc7497521a5c038ca69
BigDany1792 <dany1792dota@gmail.com>
2022-05-02 17:14:38 -0500

    style: fix pycodestyle

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    """replaces an element in a list at
+    a specific position without modifying
+    the original list"""
 
-	"""replaces an element in a list at
-	a specific position without modifying
-	the original list"""
-	
-	if (idx < 0 or idx >= len(my_list)):
-		return my_list.copy()
+    if (idx < 0 or idx >= len(my_list)):
+        return my_list.copy()
 
-	cpylist = my_list.copy()
-	cpylist[idx] = element
-	
-	return cpylist
+    cpylist = my_list.copy()
+    cpylist[idx] = element
+
+    return cpylist
