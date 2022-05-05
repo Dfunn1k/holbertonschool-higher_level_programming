d11cc5b2e4c57cf8197c981557608e2ae1d62c40
BigDany1792 <dany1792dota@gmail.com>
2022-05-04 22:45:30 -0500

    ci: added permission

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
+    """Return a list with values multiplied"""
     new_list = list(map(lambda x: x * number, my_list))
     return new_list
