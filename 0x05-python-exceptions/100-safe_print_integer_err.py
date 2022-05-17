1de31a8532fef8f1f108942281000e2522776fff
BigDany1792 <dany1792dota@gmail.com>
2022-05-16 21:09:44 -0500

    ci: added function that prints an integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return False
