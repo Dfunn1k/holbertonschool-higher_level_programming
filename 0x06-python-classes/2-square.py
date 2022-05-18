09b4e445da99af45425f4bcb7825b96a08f5805e
BigDany1792 <dany1792dota@gmail.com>
2022-05-17 19:50:03 -0500

    fix: fix problem with the compilation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,14 +9,13 @@ class Square:
     instation with optional size
     """
 
-
-def __init__(self, size=0):
-    """
-    Args:
-            arg1 (int)
-    """
-    if not type(size) is int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+    def __init__(self, size=0):
+        """
+	Args:
+		arg1 (int)
+	"""
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
