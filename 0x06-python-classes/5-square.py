c205a0a03514a07a50a07fce0270bcd58a3616ba
BigDany1792 <dany1792dota@gmail.com>
2022-05-17 21:32:15 -0500

    ci: added public instance method that print the square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Class about square"""
+
+
+class Square:
+    """
+    Define a square by:
+    private instance attribute(size)
+    instation with optional size
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize the object
+        Args:
+            arg1 (int)
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ retrieve size """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """for set it"""
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Return: the area of square
+        """
+        size = self.__size
+        return size * size
+
+    def my_print(self):
+        """
+        print the square
+        """
+        if self.size > 0:
+            for i in range(0, self.size):
+                for i in range(0, self.size):
+                    print("#", end='')
+                print("")
+        print("")
