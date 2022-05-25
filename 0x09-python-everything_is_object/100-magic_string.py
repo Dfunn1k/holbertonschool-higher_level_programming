ad4e1aa4f62962154e7576efa7e91822a0e2e1ba
BigDany1792 <dany1792dota@gmail.com>
2022-05-24 23:30:06 -0500

    ci: added tasks advanced

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(var=[0]):
+	var[0] += 1
+	return str("BestSchool, " * (var[0] - 1)) + "BestSchool"
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Low memory cost"""
+
+
+class LockedClass:
+    """Class of LockedClass"""
+    __slots__ = ['first_name']
