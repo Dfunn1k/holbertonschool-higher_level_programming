55fdfab51565e28f4c0fa7951ef53a24265e55d5
BigDany1792 <dany1792dota@gmail.com>
2022-05-29 15:31:29 -0500

    ci: added public instance method that computed the area of square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,7 +15,7 @@ class Square():
             raise ValueError("size must be >= 0")
         self.__size = size
 
-    def area(self, size):
+    def area(self):
         """
         Args:
             size (int): size of square
