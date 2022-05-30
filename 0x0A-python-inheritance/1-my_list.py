812949b872a2acfdcc2038a3cbc32ab3df715e59
BigDany1792 <dany1792dota@gmail.com>
2022-05-30 18:57:41 -0500

    ci: added new subclass and method

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""In this file """
+
+
+class MyList(list):
+    """Subclass that inherits from list"""
+    def print_sorted(self):
+        """Method that prints the list, but sorted.
+        
+        Attributes:
+            * cpy_list (list): list that copy the real list
+        """
+        cpy_list = self[:]
+        cpy_list.sort()
+        print(cpy_list)
