89a792d5d72aab6e14a0fc01d16a0be4ae884804
BigDany1792 <dany1792dota@gmail.com>
2022-05-30 15:18:05 -0500

    ci: added function that return a list of attributes and methods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""this file shows a list of the methods of an object"""
+
+
+def lookup(obj):
+    """Returns the list of available attributes and methods of an object"""
+    return dir(obj)
