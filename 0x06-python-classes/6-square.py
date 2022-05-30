2951c86e09f23a58e8b48ffe0d1eafaeedee141d
BigDany1792 <dany1792dota@gmail.com>
2022-05-29 20:34:31 -0500

    ci: added new position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+""" in this file, i will create a empty class for square"""
+
+
+from turtle import pos, position
+from typing import Tuple
+
+
+class Square():
+    """Define a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Magic mehtod for initializating with size
+        Args:
+
+            size (int): size of square
+            position (tuple): position of square
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Property to retrieve the size.
+
+        Property setter to set and check:
+            * Size must be an integer, otherwise raise a TypeError
+            * If size is less than 0, raise a ValueError
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not type(value) is int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    @property
+    def position(self):
+        """Property to retrieve the position.
+        
+        Property setter to set and check:
+            * Position must be a tuple of 2 positive integers, otherwise raise
+                a TypeError
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple)\
+        or not type(value[0]) is int \
+        or not type(value[1]) is int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        self.__position = value
+
+    def area(self):
+        """Public instance method for calculated the area for square.
+
+        Args:
+            size (int): size of square
+        Return:
+            area of square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Public instance method that prints in the stdout the square with the
+        character '#'.
+            * If size is equal to 0, print an empty line.
+
+            Args:
+                size = stored the size of instance
+        """
+        size = self.__size
+        value = self.__position
+        
+        if size != 0:
+            if value[1] > 0:
+                for i in range(0, size):
+                    print(f"{'#' * size}", end='')
+                    print("")
+            else:
+                for i in range(0, size):
+                    print(f"{' ' * value[0]}" + f"{'#' * size}", end='')
+                    print("")
+        else:
+            print("")
