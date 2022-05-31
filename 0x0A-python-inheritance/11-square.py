51e8292e2da2e31fab4072c27384bb536d94b89d
BigDany1792 <dany1792dota@gmail.com>
2022-05-30 22:11:48 -0500

    ci: new public instance method

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""In this file we'll create a subclass from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Subclass Square that inherited from Rectangle."""
+    def __init__(self, size):
+        """Instantiation with size."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Public instance method for calculated area of square.
+
+        Return:
+            The area of square."""
+        return self.__size ** 2
+
+    def __str__(self):
+        """informal string return"""
+        return f"[Square] {self.__size}/{self.__size}"
