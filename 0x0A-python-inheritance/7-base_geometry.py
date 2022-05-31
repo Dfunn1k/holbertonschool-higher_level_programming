234d820c5e44566d6f7736430ca90622d2e89f89
BigDany1792 <dany1792dota@gmail.com>
2022-05-30 21:08:23 -0500

    ci: added new class BaseGeometry

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""In this file create a class with method that not implemented"""
+
+
+class BaseGeometry:
+    """class construction in process"""
+    def area(self):
+        """Method not is implemented"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """Public instance method that validates value.
+        Args:
+            * name (str): name of the object.
+            * value (int): data integer.
+        """
+        if not type(value) is int:
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
