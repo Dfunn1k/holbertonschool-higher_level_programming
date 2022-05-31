a351a58b9622c5cbe53428b352ecd9fdb80d9442
BigDany1792 <dany1792dota@gmail.com>
2022-05-30 19:40:18 -0500

    ci: added function that check if is an instance of a class taht inherited

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -17,4 +17,4 @@ def inherits_from(obj, a_class):
         True: if there is coincidence.
         False: otherwise.
     """
-    return issubclass(obj, a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
