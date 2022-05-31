7d327eec061cae54d6fbdb4a51c066ac7ad89744
BigDany1792 <dany1792dota@gmail.com>
2022-05-30 21:24:15 -0500

    ci: added new subclass that inherited from rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""In this file we'll create a subclass from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Subclass Square that inherited from Rectangle."""
+    def __init__(self, size):
+        """Instantiation with size."""
+        Rectangle.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Public instance method for calculated area of square.
+
+        Return:
+            The area of square."""
+        return self.__size ** 2
