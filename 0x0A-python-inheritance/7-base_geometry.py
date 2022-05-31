0d792ed163c0d473c712629ba1457bda361c15a0
BigDany1792 <dany1792dota@gmail.com>
2022-05-30 21:09:16 -0500

    ci: added new class BaseGeometry

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,6 +7,7 @@ class BaseGeometry:
     def area(self):
         """Method not is implemented"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """Public instance method that validates value.
         Args:
