11a39024db5f661ec69dc614e27f494a8875ed9a
BigDany1792 <dany1792dota@gmail.com>
2022-05-30 19:10:29 -0500

    refactor: using type in return

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,19 +5,14 @@ another"""
 
 def is_same_class(obj, a_class):
     """Check if the obj is exactly an instance of the class.
-    
+
     Args:
         * obj (object): the object to be checked.
         * a_class (class): the class against which the object will be
         compared.
-        
+
     Return:
         True: if there is coincidence.
         False: otherwise.
     """
-    if a_class is object:
-        return False
-    elif isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return type(obj) is a_class
