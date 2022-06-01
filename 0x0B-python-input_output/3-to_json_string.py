a727deac9131690c90ccac12f280783e2bcf9559
BigDany1792 <dany1792dota@gmail.com>
2022-05-31 22:48:49 -0500

    ci: added function that return the JSON representation of an object2

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""To JSON string"""
+import json
+
+
+def to_json_string(my_obj):
+    """function that return the JSON representation of an object"""
+    return json.dumps(my_obj)
