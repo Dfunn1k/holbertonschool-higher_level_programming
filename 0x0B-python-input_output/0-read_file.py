bad47907130a0d4c338bce216ffbc5639dc9ee14
BigDany1792 <dany1792dota@gmail.com>
2022-05-31 20:03:49 -0500

    refactor: refactor file2

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""In this file we'll create a function that read a file and print in stdout"""
+
 
 def read_file(filename=""):
     """Function that reads a text file and prints it to stdout.
@@ -7,4 +9,4 @@ def read_file(filename=""):
         read_data (file descriptor): var where it stored a file"""
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data)
+    print(read_data, end="")
