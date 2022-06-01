b454c6e1ec1f947c475d0a182022ed3e65f15651
BigDany1792 <dany1792dota@gmail.com>
2022-06-01 13:02:47 -0500

    ci: added new script

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Load, add, save"""
 from sys import argv
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
