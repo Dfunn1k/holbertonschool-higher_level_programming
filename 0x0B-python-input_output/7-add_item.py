f0223a9160cf5985ba136d7e20ba5034113e8df0
BigDany1792 <dany1792dota@gmail.com>
2022-06-01 13:03:22 -0500

    ci: added new script

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,26 +5,12 @@ from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-def append_Args(argv, list1=[]):
-    """function that stores the arguments in a list except the first.
-
-    Attributes:
-        lenght (int): lenght of argv
-    Args:
-        argv (list): Command line arguments
-        list1 (list): List
-    """
-    lenght = len(argv)
-
-    for i in range(1, lenght):
-        list1.append(argv[i])
-
-
 try:
-    jsonlist = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    jsonlist = []
+    json_list = load_from_json_file('add_item.json')
+except:
+    json_list = []
 
-append_Args(argv, jsonlist)
-save_to_json_file(jsonlist, 'add_item.json')
+for i in argv[1:]:
+    json_list.append(i)
+
+save_to_json_file(json_list, 'add_item.json')
