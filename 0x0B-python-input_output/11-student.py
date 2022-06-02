345387220e04e9d7b91ce230a6215440b17a2657
BigDany1792 <dany1792dota@gmail.com>
2022-06-01 22:11:14 -0500

    ci: added method

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Student to JSON with filter"""
+
+
+class Student:
+    """Defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the data"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """AI is creating summary for to_json"""
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for i in attrs:
+            try:
+                new_dict[i] = self.__dict__[i]
+            except Exception:
+                pass
+        return new_dict
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for i in json:
+            self.__dict__.update({i: json[i]})
