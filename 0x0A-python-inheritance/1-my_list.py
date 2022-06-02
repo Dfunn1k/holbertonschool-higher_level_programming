a1a2670da3f8ab8f8a006adc2bbd194ea9d88d29
BigDany1792 <dany1792dota@gmail.com>
2022-06-01 22:39:49 -0500

    style: pass pycodestyle

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,7 +6,7 @@ class MyList(list):
     """Subclass that inherits from list"""
     def print_sorted(self):
         """Method that prints the list, but sorted.
-        
+
         Attributes:
             * cpy_list (list): list that copy the real list
         """
