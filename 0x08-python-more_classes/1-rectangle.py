0d8ad7106b7cf52e618b02d29f3c54830b88e332
BigDany1792 <dany1792dota@gmail.com>
2022-06-26 15:04:56 -0500

    ci: added propertys set and getter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -30,6 +30,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -47,3 +48,4 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__height = value
