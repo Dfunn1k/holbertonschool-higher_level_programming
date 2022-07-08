7cdc1b26b7302797944683f2224ddeefd3652990
BigDany1792 <dany1792dota@gmail.com>
2022-07-07 22:11:11 -0500

    fix: typo

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
-i-- This script creates a table called first_table in the current database in your MySQL server.
+-- This script creates a table called first_table in the current database in your MySQL server.
 CREATE TABLE IF NOT EXISTS first_table(id INT,name VARCHAR(256));
