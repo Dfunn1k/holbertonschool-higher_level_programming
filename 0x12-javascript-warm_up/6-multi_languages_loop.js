6b7fbf6ca335b8c098af2367608f18c8605520b7
BigDany1792 <dany1792dota@gmail.com>
2022-08-02 17:04:58 -0500

    ci: added script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const multiLines = ['C is fun','Python is cool','JavaScript is amazing'];
+const multiLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-multiLines.forEach((val) => {
-	console.log(val);
-})
+for (let i = 0; i < multiLines.length; i++) {
+  console.log(multiLines[i]);
+}
