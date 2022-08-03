a10ef56daec990cac97863d3d596c88a941dcddd
BigDany1792 <dany1792dota@gmail.com>
2022-08-02 21:36:22 -0500

    style: semistandard was corrected

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor (w, h) {
-	  if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
-		this.width = w;
-		this.height = h;
-	  }
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print(){
-		let i, char='X';
-		for (i = 0; i < this.height; i++) {
-			console.log(char.repeat(this.width));
-	}
+  print () {
+    let i; const char = 'X';
+    for (i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
+    }
   }
 }
 module.exports = Rectangle;
