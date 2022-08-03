b1f8b0bac64bf2ef674ba561d418917f35788466
BigDany1792 <dany1792dota@gmail.com>
2022-08-02 20:05:19 -0500

    fix: fix flows in the code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,19 +7,15 @@ let i = 2;
 
 if (args[2] === undefined) {
   console.log(0);
-}
-
-if (len === 3) {
+} else if (len === 3) {
   console.log(0);
+} else {
+  while (i < len) {
+    newArray.push(args[i]);
+    i++;
+  }
+  newArray.sort((a, b) => {
+    return b - a;
+  });
+  console.log(newArray[1]);
 }
-
-while (i < len) {
-  newArray.push(args[i]);
-  i++;
-}
-
-newArray.sort((a, b) => {
-  return b - a;
-});
-
-console.log(newArray[1]);
