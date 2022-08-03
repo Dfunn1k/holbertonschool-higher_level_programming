ef920a5c0521eec56672395d9386be4f9c1a8ada
BigDany1792 <dany1792dota@gmail.com>
2022-08-02 19:07:48 -0500

    ci: added script that prints the square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const process = require('process');
+const args = process.argv;
+const size = parseInt(args[2]);
+let i; const char = 'x';
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (i = 0; i < size; i++) {
+    console.log(char.repeat(size));
+  }
+}
