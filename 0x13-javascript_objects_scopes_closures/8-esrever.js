49dc9ac0d09519e39fdd567d0839c7a7f74bf055
BigDany1792 <dany1792dota@gmail.com>
2022-08-02 23:50:01 -0500

    Ci: logic was corrected

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-exports.esrever = (list) => {
-  const newList = [];
-  const len = list.length;
-  let i;
-  while (len) {
-    newList.push(list.pop());
-  }
-  return (newList);
+exports.esrever = function (list) {
+  const newArray = [];
+  list.forEach((item) => newArray.unshift(item));
+  return (newArray);
 };
