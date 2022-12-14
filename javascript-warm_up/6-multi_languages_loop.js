755154bbdd6391cf80cc22a317a62f3710c46e3d
BigDany1792 <dany1792dota@gmail.com>
2022-12-14 13:17:39 -0500

    fix: typo was corrected!

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const data = ['C is fun', 'Python is cool', 'JavaScript is Amazing'];
+const data = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const mssg of data) {
   console.log(mssg);
 }
