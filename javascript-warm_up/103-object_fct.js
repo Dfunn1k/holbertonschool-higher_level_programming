cda954e4d9230eac81e0fbf79ab1852521f1453a
BigDany1792 <dany1792dota@gmail.com>
2022-12-14 17:32:24 -0500

    ci: added task 103

---
+++ javascript-warm_up/103-object_fct.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+function incr () { this.value += 1; }
+myObject.incr = incr;
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
