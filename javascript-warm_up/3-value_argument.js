037c9ea62b6dca841ba967abbf0b713cbba57f4b
BigDany1792 <dany1792dota@gmail.com>
2022-12-14 15:26:50 -0500

    REFACTOR: delete arrow funtion, add console and flow control if...else

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -4,9 +4,5 @@ const { argv } = require('process');
 if (argv[2] === undefined) {
   console.log('No argument');
 } else {
-  const sliceArrayArguments = argv.slice(2);
-
-  sliceArrayArguments.forEach((argument) => {
-    console.log(argument);
-  });
+  console.log(argv[2]);
 }
