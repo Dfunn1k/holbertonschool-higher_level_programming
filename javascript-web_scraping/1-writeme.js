9bd915a467009aee9b65d5ec421a33e29c2d1c09
BigDany1792 <dany1792dota@gmail.com>
2022-12-16 17:04:38 -0500

    ci: added task 01

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+const { argv } = require('process');
+const pathFile1 = argv[2];
+const data = argv[3] + '\n';
+
+try {
+  fs.writeFileSync(pathFile1, data, { flag: 'a' }, 'utf8');
+} catch (error) {
+  console.error(error);
+}
