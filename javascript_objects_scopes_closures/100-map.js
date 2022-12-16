74267ce1e1f6c2a5a63efd915fb46ea22260cad8
BigDany1792 <dany1792dota@gmail.com>
2022-12-15 20:25:23 -0500

    FIX: fix bug when the element be multiplied with the index, if in the list exist two element with the same value

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 const oldList = require('./100-data').list;
-const newList = oldList.map(n => n * oldList.indexOf(n));
-console.log(oldList);
-console.log(newList);
+const newList = oldList.map((n, i) => n * i);
+console.log(`${oldList}\n${newList}`);
