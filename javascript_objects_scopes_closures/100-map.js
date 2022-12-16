8afd9459d9127ecf4cfd071276c23a07313f6653
BigDany1792 <dany1792dota@gmail.com>
2022-12-15 20:29:03 -0500

    feat: format output was corrected!

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const oldList = require('./100-data').list;
 const newList = oldList.map((n, i) => n * i);
-console.log(`${oldList}\n${newList}`);
+console.log(oldList);
+console.log(newList);
