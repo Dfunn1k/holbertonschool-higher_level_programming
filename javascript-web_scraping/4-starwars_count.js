a2c8bdf16d44d5f5b91247bd417c9cb2d389b71b
BigDany1792 <dany1792dota@gmail.com>
2022-12-16 20:43:51 -0500

    ci: added task 04

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const { argv } = require('process');
-const elementsearch = 'https://swapi-api.hbtn.io/api/people/18/';
 let count = 0;
 
 request.get(argv[2], (error, response, body) => {
@@ -10,7 +9,7 @@ request.get(argv[2], (error, response, body) => {
   } else {
     const dataDeserialization = JSON.parse(response.body);
     for (const film of dataDeserialization.results) {
-      for (const peopleFilm of film.characters) { if (peopleFilm === elementsearch) { count++; } }
+      for (const peopleFilm of film.characters) { if (peopleFilm.includes('18')) { count++; } }
     }
     console.log(count);
   }
