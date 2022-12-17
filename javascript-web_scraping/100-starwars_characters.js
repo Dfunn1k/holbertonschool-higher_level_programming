91d55ea1874796d9dc00db6b5407441d283e0628
BigDany1792 <dany1792dota@gmail.com>
2022-12-17 00:24:05 -0500

    ci added task 100 and 101

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,23 +1,13 @@
 #!/usr/bin/node
-const axios = require('axios');
-const { argv } = require('process');
-const id = argv[2];
+const { get } = require('axios').default;
+const id = process.argv[2];
 const baseUrl = `https://swapi-api.hbtn.io/api/films/${id}`;
 
-axios.get(baseUrl)
-  .then((response) => {
-    const dataDeserialization1 = response.data;
-    for (const character of dataDeserialization1.characters) {
-      axios.get(character)
-        .then((response) => {
-          const dataDeserialization2 = response.data;
-          console.log(dataDeserialization2.name);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
+get(baseUrl)
+  .then(({ data: { characters } }) => {
+    characters.forEach(character => {
+      get(character)
+        .then(({ data: { name } }) => console.log(name));
+    });
   })
-  .catch((error) => {
-    console.error(error);
-  });
+  .catch((err) => console.error(err));
