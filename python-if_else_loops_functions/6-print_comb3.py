a93ceab79a51870716ac2645a39dfcf36f662052
dfunn1k <danychavezdev@gmail.com>
2024-01-04 17:58:02 -0500

    feat: added new script for print numbers and loops

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(9):
+    for j in range(i+1, 10):
+        if i == 8 and j == 9:
+            print('{}{}'.format(i, j))
+        else:
+            print('{}{}, '.format(i, j), end="")
