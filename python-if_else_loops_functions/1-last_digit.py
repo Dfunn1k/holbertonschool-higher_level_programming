ad7f9ed5b4ce1b024098a4d76231d5552bfe2d84
dfunn1k <danychavezdev@gmail.com>
2024-01-04 01:23:31 -0500

    feat: added new directory for practice loops

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number < 0:
+    last_digit = (abs(number) % 10) * -1
+else:
+    last_digit = number % 10
+
+if last_digit == 0:
+    print(f'Last digit of {number} is 0 and is 0')
+elif last_digit > 5:
+    print(f'Last digit of {number} is {last_digit} and is greater than 5')
+else:
+    print(f'Last digit of {number} is {last_digit} and '
+          f'is less than 6 and not 0')
