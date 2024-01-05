78edfb5ba5b69a5cf292e24aa1a1531ddf0a584b
dfunn1k <danychavezdev@gmail.com>
2024-01-04 23:45:23 -0500

    feat: error with spaces in output was corrected

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        print('{}'.format(chr(ord(char.lower()) - 32)), end='')
+        char_ascii = ord(char)
+        if 96 < char_ascii < 123:
+            char_ascii -= 32
+        print('{}'.format(chr(char_ascii)), end='')
     print('')
