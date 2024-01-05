b859d7e1584086e6f0411138d3c0ef17a614e838
dfunn1k <danychavezdev@gmail.com>
2024-01-04 23:30:52 -0500

    feat: error in output expected was corrected

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        if char == str[-1]:
-            print('{}'.format(chr(ord(char.lower()) - 32)))
-        else:
-            print('{}'.format(chr(ord(char.lower()) - 32)), end='')
+        print('{}'.format(chr(ord(char.lower()) - 32)), end='')
+    print('')
