5afc080052e3504356ad151e68d4fa48d83b2d1b
dfunn1k <danychavezdev@gmail.com>
2024-01-06 13:10:06 -0500

    feat: change logic

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -4,6 +4,10 @@ if __name__ == '__main__':
     from sys import argv, exit
     from calculator_1 import add, sub, div, mul
 
+    if len(argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+
     operator = argv[2]
     num1 = int(argv[1])
     num2 = int(argv[3])
@@ -14,10 +18,6 @@ if __name__ == '__main__':
         '*': mul,
         '/': div
     }
-
-    if len(argv) != 4:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        exit(1)
 
     if operator not in operations.keys():
         print('Unknown operator. Available operators: +, -, * and /')
