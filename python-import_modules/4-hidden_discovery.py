9668a1edae2027bb2ab7ff5b548c932f235b1608
dfunn1k <danychavezdev@gmail.com>
2024-01-06 12:45:29 -0500

    feat: change logic

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     import hidden_4
     names = dir(hidden_4)
     for name in names:
-        if not name.endswith('__'):
+        if not name.startswith('__'):
             print(name)
