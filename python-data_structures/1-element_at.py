8aa46830cb849f881a5161cf1f27ee29e947ff34
dfunn1k <danychavezdev@gmail.com>
2024-01-07 00:53:09 -0500

    feat: fix typo

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    lenght = len(my_list)
-    if idx < 0:
-        return None
-
-    if idx > lenght:
+    length = len(my_list)
+    if (idx < 0) or (idx >= length):
         return None
 
     return my_list[idx]
