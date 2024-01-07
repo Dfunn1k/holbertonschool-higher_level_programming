edcf0354839b56fd9886c90adc5dc1289a4df589
dfunn1k <danychavezdev@gmail.com>
2024-01-07 00:54:11 -0500

    feat: change logic

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@
 def replace_in_list(my_list, idx, element):
     length = len(my_list)
 
-    if (idx < 0) or (idx - 1 > length):
+    if (idx < 0) or (idx >= length):
         return my_list
 
     my_list[idx] = element
