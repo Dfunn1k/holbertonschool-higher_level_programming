212cb89e79c77510e5f618c714fd8439810fdc91
dfunn1k <danychavezdev@gmail.com>
2024-01-07 16:15:52 -0500

    feat: change logic

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     length = len(my_list)
 
-    if (idx < 0) or (idx > length):
+    if (idx < 0) or (idx >= length):
         return my_list
 
     del my_list[idx]
