70de54acff6257ba0e5a777b63428ddba4f229ad
dfunn1k <danychavezdev@gmail.com>
2024-01-07 15:57:26 -0500

    feat: fix logic

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_int = 0
-
     if not my_list:
         return None
 
-    for num in my_list:
-        if num > max_int:
-            max_int = num
-
-    return max_int
+    my_list_copy = my_list.copy()
+    my_list_copy.sort()
+    return my_list_copy[-1]
