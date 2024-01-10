0e6a70872038cf1a71adf0662958f493aca89572
dfunn1k <danychavezdev@gmail.com>
2024-01-10 18:31:29 -0500

    feat: pass pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     if (type(roman_string) is not str) or (roman_string is None):
         return 0
 
-    number_romans = {
+    number_roman = {
         'I': 1,
         'V': 5,
         'X': 10,
@@ -19,10 +19,10 @@ def roman_to_int(roman_string):
 
     for i in range(length):
         if roman_string[i] == roman_string[-1]:
-            total += number_romans[roman_string[i]]
-        elif number_romans[roman_string[i]] < number_romans[roman_string[i + 1]]:
-            total -= number_romans[roman_string[i]]
+            total += number_roman[roman_string[i]]
+        elif number_roman[roman_string[i]] < number_roman[roman_string[i + 1]]:
+            total -= number_roman[roman_string[i]]
         else:
-            total += number_romans[roman_string[i]]
+            total += number_roman[roman_string[i]]
 
     return total
