f1513f1459c5985c0797a4bd95138742b34e3120
dfunn1k <danychavezdev@gmail.com>
2024-01-10 19:57:51 -0500

    feat: added method for class square

---
+++ python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Its is a module for learn about class in python"""
+
+
+class Square:
+    """A class that build square"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
