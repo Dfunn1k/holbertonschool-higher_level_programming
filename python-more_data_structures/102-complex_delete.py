4f0415ae0511e0f3c88bee90f438edb75d3a563a
dfunn1k <danychavezdev@gmail.com>
2024-01-10 19:55:38 -0500

    feat: added new logic

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
+    keys_deleted = []
+
     for k, v in a_dictionary.items():
         if v == value:
-            del a_dictionary[k]
+            keys_deleted.append(k)
+
+    for key in keys_deleted:
+        del a_dictionary[key]
+
+    return a_dictionary
