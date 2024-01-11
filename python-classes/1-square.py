83ffecdae315ffdabae6a54f712266a6e5619def
dfunn1k <danychavezdev@gmail.com>
2024-01-10 19:43:28 -0500

    feat: added initialization with size in class square

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Its is a module for learn about class in python"""
+
+
+class Square:
+    """A class that build square"""
+    def __init__(self, size):
+        self.__size = size
