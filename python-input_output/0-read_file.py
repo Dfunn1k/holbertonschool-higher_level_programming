0b2e0b7f1b37f28450bb1998c734c76b0145338d
dfunn1k <danychavezdev@gmail.com>
2024-01-14 20:20:08 -0500

    feat: added new logic

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""Read_file"""
+"""In this file we'll create a function that read a file and print in stdout"""
 
 
 def read_file(filename=""):
-    """
-    Function that print the content in file
+    """Function that reads a text file and prints it to stdout.
     Args:
-        filename: file that will be read
-    """
-    with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read())
+        filename (file descriptor): var where it stored a file"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+    print(read_data, end="")
