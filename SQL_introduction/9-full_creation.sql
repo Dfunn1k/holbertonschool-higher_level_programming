41e2650b6734bbd09941e5a7df0dc8d9a595e7ad
dfunn1k <danychavezdev@gmail.com>
2024-01-17 13:13:29 -0500

    feat: added script that create table and insert any rows

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- Script that create a table and insert rows
+CREATE TABLE IF NOT EXISTS `second_table`(
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO `second_table` VALUES
+                               (1, 'John', 10),
+                               (2, 'Alex', 3),
+                               (3, 'Bob', 14),
+                               (4, 'George', 8);
