c9b18c600ce6cd5ea3e8bea3766020673ced8ef1
dfunn1k <danychavezdev@gmail.com>
2024-01-17 13:40:29 -0500

    feat: fix typo

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- Script that prints description table
+-- This script prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 SHOW CREATE TABLE first_table;
