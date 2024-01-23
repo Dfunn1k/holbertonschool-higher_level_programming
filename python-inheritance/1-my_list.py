d309c4022db7c22934af534753757e85d567d3cc
dfunn1k <danychavezdev@gmail.com>
2024-01-23 12:29:10 -0500

    feat: added function that print the list in order

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 
-
 class MyList(list):
-    def __init__(self):
-        super().__init__()
+    """
+    MyList is a class that extends the built-in list class in Python.
+
+    Methods
+    -------
+    print_sorted():
+        Prints the elements of the list in a sorted order.
+    """
 
     def print_sorted(self):
+        """
+        Method to print the elements of the list in a sorted order.
+
+        The sorted() function is a built-in Python function that takes an
+        iterable and returns a new sorted list of the elements in the iterable.
+        """
         print(sorted(self))
