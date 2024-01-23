05c393dbb09fb71a752ed11ef12d18cc956e5954
dfunn1k <danychavezdev@gmail.com>
2024-01-23 13:27:37 -0500

    feat: change logic

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,43 +1,6 @@
 #!/usr/bin/python3
 """Geometry module"""
-
-
-class BaseGeometry:
-    def area(self):
-        """
-        Calculates the area of the geometric shape.
-
-        This method should be implemented by subclasses of BaseGeometry.
-
-        Raises
-        ------
-        Exception
-            If the method is not implemented by a subclass.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates that a value is a positive integer.
-
-        Parameters
-        ----------
-        name : str
-            The name of the value, used in error messages.
-        value : int
-            The value to validate.
-
-        Raises
-        ------
-        TypeError
-            If the value is not an integer.
-        ValueError
-            If the value is not greater than 0.
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
