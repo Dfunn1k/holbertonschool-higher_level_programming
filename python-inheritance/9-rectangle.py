f94a77264dd9a54c27d53c38b2448272ef187756
dfunn1k <danychavezdev@gmail.com>
2024-01-23 18:35:29 -0500

    feat: fix typo

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -61,4 +61,4 @@ class Rectangle(BaseGeometry):
             "[Rectangle] <width>/<height>".
 
         """
-        return f"[Rectangle] <{self.__width}>/<{self.__height}>"
+        return f"[Rectangle] {self.__width}/{self.__height}"
