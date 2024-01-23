0ec4bf9b11126c95bb8307c68e0c1e90d8306d28
dfunn1k <danychavezdev@gmail.com>
2024-01-23 13:15:13 -0500

    feat: change logic

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -34,7 +34,7 @@ class BaseGeometry:
         ValueError
             If the value is not greater than 0.
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
