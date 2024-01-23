1fba5ec4ae020403a92fe37b9a237ab1b15cf9aa
dfunn1k <danychavezdev@gmail.com>
2024-01-23 12:35:42 -0500

    feat: added function that check if an object is instance of the class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Exact same object"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if an object is an instance of a specified class.
+
+    Parameters
+    ----------
+    obj : object
+        The object to check.
+    a_class : class
+        The class to check against.
+
+    Returns
+    -------
+    bool
+        True if the object is an instance of the specified class, False otherwise.
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
